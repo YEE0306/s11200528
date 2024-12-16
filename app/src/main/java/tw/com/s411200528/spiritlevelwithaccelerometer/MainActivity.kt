1a4bd1614baa5539a6c98ba27a10caa0188b4988
廖翊婷 <a0932111445@gmail.com>
2024-12-16 15:04:35 +0800

    v0第一次

---
+++ app/src/main/java/tw/com/s411200528/spiritlevelwithaccelerometer/MainActivity.kt
@@ -0,0 +1,52 @@
+package tw.com.s411200528.spiritlevelwithaccelerometer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.com.s411200528.spiritlevelwithaccelerometer.ui.theme.SpiritLevelWithAccelerometerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SpiritLevelWithAccelerometerTheme {
+                /*
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+                */
+                SpiritLevelView()
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SpiritLevelWithAccelerometerTheme {
+        Greeting("Android")
+    }
+}
